f05fce6e73c92e33c2c6e6bb67742f9091e372f8
Prem <premkhodke.99@gmail.com>
2024-03-23 23:07:01 +0530

    recipe-card/home/css

---
+++ src/context/index.jsx
@@ -0,0 +1,32 @@
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children }) {
+
+    const [searchParam, setSearchParam] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [recipeList, setRecipeList] = useState([])
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
+
+            const data = await res.json();
+
+            if (data?.data?.recipes) {
+                setRecipeList(data?.data?.recipes)
+            }
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoading(false)
+            setSearchParam('')
+        }
+    }
+
+    return <GlobalContext.Provider value={{ searchParam, setSearchParam, loading, recipeList, handleSubmit }}>
+        {children}
+    </GlobalContext.Provider>
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,3 +1,24 @@
-export default function Home(){
-     return <>Home</>
+import { useContext } from "react"
+import { GlobalContext } from "../../context"
+import RecipeItem from "../../components/recipe-item/RecipeItem";
+
+export default function Home() {
+
+     const { recipeList, loading } = useContext(GlobalContext);
+
+     if (loading) {
+          return <h5>Loading...Please wait!</h5>
+     }
+
+     return <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+          {
+               recipeList && recipeList.length > 0 ?
+                    recipeList.map((item, index) => <RecipeItem item={item} />
+                    ) : (
+                         <div>
+                              <p className="lg:text-4xl text-xl text-center text-black font-extrabold">Nothing to show. please search something</p>
+                         </div>
+                    )
+          }
+     </div>
 }
